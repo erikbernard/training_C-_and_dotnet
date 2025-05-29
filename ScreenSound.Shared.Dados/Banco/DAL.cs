a868316b3ddd53683acd1f13f8ff89a5b960e2d9
erikbernard <erikbrsilva@gmail.com>
2025-05-29 10:35:26 -0300

    feat: segregation of the project into data and models

--- ScreenSound.Shared.Dados/Banco/DAL.cs
+++ ScreenSound.Shared.Dados/Banco/DAL.cs
@@ -8,9 +8,9 @@ using System.Threading.Tasks;
 
 namespace ScreenSound.Banco
 {
-    internal class DAL<T> where T : class
+    public class DAL<T> where T : class
     {
-        protected readonly ScreenSoundContext _context;
+        private  readonly ScreenSoundContext _context;
 
         public DAL(ScreenSoundContext context)
         {
--- ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
+++ ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
@@ -3,18 +3,19 @@ using ScreenSound.Modelos;
 
 namespace ScreenSound.Banco
 {
-    internal class ScreenSoundContext: DbContext
+    public class ScreenSoundContext: DbContext
     {
-        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        private string _connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
         public DbSet<Artista> Artistas { get; set; }
         public DbSet<Musica> Musicas { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder
-                .UseSqlServer(connectionString)
+                .UseSqlServer(_connectionString)
                 .UseLazyLoadingProxies();
         }
 
     }
 }
+ 
