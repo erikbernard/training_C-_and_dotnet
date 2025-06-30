6a6cacf068835c92235efb457b220e6b66b2b278
erikbernard <erikbrsilva@gmail.com>
2025-06-30 13:15:22 -0300

    feat: separation of responsibilities, creation of api endpointsfeat: datebase and migrations to api

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -1,6 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using ScreenSound.API.Endpoints;
+using ScreenSound.Banco;
+using ScreenSound.Modelos;
+using ScreenSound.Shared.Modelos.Modelos;
+using System.Data.SqlTypes;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+builder.Services.AddDbContext<ScreenSoundContext>();
+builder.Services.AddTransient<DAL<Artista>>();
+builder.Services.AddTransient<DAL<Musica>>();
+builder.Services.AddTransient<DAL<Genero>>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.AddEndpointsArtistas();
+app.AddEndpointsMusicas();
+app.AddEndpointsGeneros();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
+
