2e72e53abda46a1cc8cd56aa8c8d8f1576729304
erikbernard <erikbrsilva@gmail.com>
2025-07-28 14:02:58 -0300

    feat: add service artista

--- ScreenSound.Web/Services/ArtistaAPI.cs
+++ ScreenSound.Web/Services/ArtistaAPI.cs
@@ -1,4 +1,6 @@
-﻿using ScreenSound.Web.Response;
+﻿using ScreenSound.Web.Pages;
+using ScreenSound.Web.Requests;
+using ScreenSound.Web.Response;
 using System.Net.Http.Json;
 
 namespace ScreenSound.Web.Services
@@ -14,6 +16,22 @@ namespace ScreenSound.Web.Services
         public async Task<ICollection<ArtistaResponse>?> GetArtistasAsync()
         {
             return await _httpClient.GetFromJsonAsync<ICollection<ArtistaResponse>>("artistas");
+        }
+        public async Task AddArtistaAsync(ArtistasRequest artistas)
+        {
+            await _httpClient.PostAsJsonAsync("artistas", artistas);
+        }
+        public async Task UpdateArtistaAsync(ArtistaRequestEdit artistas)
+        {
+            await _httpClient.PutAsJsonAsync($"artistas/{artistas.Id}", artistas);
+        }
+        public async Task DeleteArtistaAsync(int id)
+        {
+            await _httpClient.DeleteAsync($"artistas/{id}");
+        }
+        public async Task<ArtistaResponse?> GetArtistaPorNomeAsync(string nome)
+        {
+            return await _httpClient.GetFromJsonAsync<ArtistaResponse>($"artistas/{nome}");
         }
     }
 }
